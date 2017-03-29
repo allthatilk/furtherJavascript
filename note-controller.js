9da2aa09b26aaf1646f1b6f478b054eba16c72ca
allthatilk <imogenkutz@hotmail.com>
2017-03-29 16:38:46 +0100

    Spiked displaynote list on index.html. Currently working

--- note-controller.js
+++ note-controller.js
@@ -1,5 +1,26 @@
-window.onload = (function changeToHowdy() {
+'strict mode'
+
+;(function(exports) {
+  function changeText(string) {
   var text = document.getElementById("app")
   console.log(text)
-  text.innerHTML = "Howdy"
-})
+  text.innerHTML = string
+  }
+  exports.changeText = changeText
+})(this)
+
+changeText("Howdy")
+
+
+;(function(exports) {
+  function NoteController(listModel) {
+    this.listModel = listModel
+  }
+
+  NoteController.prototype.getHTMLList = function(htmlList) {
+    changeText(htmlList)
+  }
+
+  exports.NoteController = NoteController
+  
+})(this)

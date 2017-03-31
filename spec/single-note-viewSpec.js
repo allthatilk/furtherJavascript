a1204b1149a8257a5e20ef3230f23f2147499c27
allthatilk <imogenkutz@hotmail.com>
2017-03-31 19:10:41 +0100

    Added a method to single note view that converts a note to HTML. Completed challenge 7. All tests passing.

--- spec/single-note-viewSpec.js
+++ spec/single-note-viewSpec.js
@@ -5,5 +5,14 @@ test({
     var note = new Note("A single note")
     var snv = new SingleNoteView(note)
     assert.isEqual(snv.singleNote, note)
+  },
+
+  methodConvertsNoteToHtml: function() {
+    var note = new Note("This will become HTML")
+    var snv = new SingleNoteView(note)
+    assert.isEqual(
+      snv.convertNoteToHtml(),
+      "<div>This will become HTML</div>"
+    )
   }
 })
